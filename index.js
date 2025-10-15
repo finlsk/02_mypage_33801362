ed88709285800e44f77b89ae91b4ecc0d46b5b29
fin <finlaystandring@gmail.com>
2025-10-15 16:08:56 +0100

    Added a more functional webpage

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const port = 8000;
 
 http.createServer(function(req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello World! small change :)");
+    res.end(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>PAGE!!!!!</title>
+        </head>
+        <body>
+            <h1>On the VM?!?!</h1>
+            <p>yes this is on the VM</p>
+        </body>
+        </html>
+        `);
     }).listen(port, function() {
         console.log(`Node server is running on ${port}...`);
 });
